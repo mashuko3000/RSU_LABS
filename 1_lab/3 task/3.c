82cacfd31f8dc136c2e310220cc38c1a7da0d0ab
mashuko <maria10vanilla@gmail.com>
2025-09-15 23:32:53 +0300

    Add files via upload

--- 1_lab/3 task/3.c
+++ 1_lab/3 task/3.c
@@ -8,20 +8,28 @@
 
 int copy_data (const char* source_path, const char* dest_path)
 {
-    FILE *source_file = fopen(source_path, "rb");
+    FILE *source_file;
+    FILE *dest_file;
+    size_t capacity = 4096;
+    char* buffer;
+    size_t bytes_read;
+    char* temp;
+    size_t bytes_written;
+
+
+    source_file = fopen(source_path, "rb");
     if (!source_file)
     {
         return ERR_OPEN_FILE;
     }
 
-    FILE *dest_file = fopen(dest_path, "wb");
+    dest_file = fopen(dest_path, "wb");
     if (!dest_file)
     {
         return ERR_OPEN_FILE;
     }
 
-    size_t capacity = 4096;
-    char* buffer = malloc(sizeof(char) * capacity);
+    buffer = (char*)malloc(sizeof(char) * capacity);
     if (buffer == NULL)
     {
         fclode(source_file);
@@ -29,11 +37,10 @@ int copy_data (const char* source_path, const char* dest_path)
         return ERR_MALLOC;
     }
 
-    size_t bytes_read;
     while((bytes_read = fread(buffer, 1, capacity, source_file)) > 0)
     {
         if(bytes_read == capacity){
-            char* temp = realloc(buffer, sizeof(char) * 2 * capacity);
+            temp = (char*)realloc(buffer, sizeof(char) * 2 * capacity);
             if (temp == NULL)
             {
                 free(buffer);
@@ -44,7 +51,7 @@ int copy_data (const char* source_path, const char* dest_path)
             buffer = temp;
             capacity*=2;
         }
-        size_t bytes_written = fwrite(buffer, 1, bytes_read, dest_file);
+        bytes_written = fwrite(buffer, 1, bytes_read, dest_file);
         if (bytes_read != bytes_written)
         {
             free(buffer);
@@ -69,6 +76,7 @@ int copy_data (const char* source_path, const char* dest_path)
 
 int main(int argc, char* argv[])
 {
+    int result;
     if (argc != 3)
     {
         return ERR_INVALID;
@@ -77,6 +85,6 @@ int main(int argc, char* argv[])
     const char* source_path = argv[1];
     const char* dest_path = argv[2];
 
-    int result = copy_data(source_path, dest_path);
+    result = copy_data(source_path, dest_path);
     return result;
 }
