cd6debb19e568f36a69bead84ad8dcee73cb13e0
mashuko <maria10vanilla@gmail.com>
2025-10-10 16:46:23 +0300

    Delete 2_lab directory

--- 2_lab/17_task.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE public.logs(
-	log_id BIGSERIAL PRIMARY KEY,
-	log_data_time TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
-	log_context VARCHAR(100) NOT NULL,
-	log_content TEXT
-);
